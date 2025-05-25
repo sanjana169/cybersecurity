471cc40b7dd16796abfb65df29cd9d15ef208aa1
Cybersecurity <sanjanaradadiya2@gmail.com>
2025-05-25 17:43:27 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";       // default for XAMPP/WAMP
-$password = "";           // default is empty
+$username = "root";       
+$password = "";          
 $dbname = "cybersecurity";
 
 // Create connection
